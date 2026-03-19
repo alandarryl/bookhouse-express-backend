9f689500180e2296ffd8044901e56af40d24bf93
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-03-19 12:35:37 +0100

    add a few more route

--- routes/bookRoutes.js
+++ routes/bookRoutes.js
@@ -4,7 +4,14 @@ const express = require('express');
 
 const router = express.Router();
 
-const {getAllBook, getOneBook, createBook} = require('../controller/bookController');
+const {
+        createBook,
+        getAllBook,
+        getOneBook,
+        getUserBooks,
+        updateBook,
+        deleteBook
+} = require('../controller/bookController');
 
 
 //route to get all book
@@ -17,6 +24,14 @@ router.route("/create").post(createBook);
 //route get one book
 router.route("/getOne/:id").get(getOneBook);
 
+//route to update
+router.route("/update").put(updateBook);
+
+//route to delete
+router.route("/delete").delete(deleteBook);
+
+// Route pour le profil d'un utilisateur
+router.get('/user/:userId', getUserBooks);
 
 module.exports = router
 
