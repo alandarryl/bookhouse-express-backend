52c4824ffe8501f5eabe369fbdc59ea2f28fc207
okanajonathan2@gmail.com <okanajonathan2@gmail.com>
2026-04-11 18:11:40 +0200

    fix and check that user and books api and routes worked, confirm

--- models/User.js
+++ models/User.js
@@ -29,22 +29,26 @@ const userSchema = new mongoose.Schema(
     },{timestamps: true}
 );
 
-//method to compare the password enter withthe hach version
-userSchema.methods.matchPassword = async function (enteredPassword){
+/**
+ * Méthode pour comparer le mot de passe saisi avec la version hachée
+ */
+userSchema.methods.matchPassword = async function (enteredPassword) {
     return await bcrypt.compare(enteredPassword, this.password);
 };
 
-//Middleware mongoose : hach the password before saving it
-userSchema.pre('save', async function (next){
-    if (!this.isModified('password')) return next();
-
-    try {
-        const salt = await bcrypt.genSalt(10);
-        this.password = await bcrypt.hash(this.password, salt);
-        next();
-    } catch (error) {
-        next(error); // handle error
+/**
+ * Middleware Mongoose (Hook) : Hache le mot de passe avant la sauvegarde
+ * Note : Pas de 'next' ici car c'est une fonction async.
+ */
+userSchema.pre('save', async function () {
+    // Si le mot de passe n'est pas modifié, on passe à la suite
+    if (!this.isModified('password')) {
+        return; 
     }
+
+    // Hachage du mot de passe
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
 });
 
 module.exports = mongoose.model("User", userSchema);
